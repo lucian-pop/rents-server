3de2c76dc2e160194446832b6f3e8d81a2d137e9
Lucian Pop <lucian.pop.play@gmail.com>
2019-08-09 11:58:01 +0300

    Integrated CORS to allow request from fizboapp.co.uk

--- rents-server/src/main/java/ro/fizbo/rents/ResourcesRegistrar.java
+++ rents-server/src/main/java/ro/fizbo/rents/ResourcesRegistrar.java
@@ -4,12 +4,14 @@ import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import ro.fizbo.rents.webservice.jsonprovider.GsonMessageBodyHandler;
+import ro.fizbo.rents.webservice.response.CustomResponseFilter;
 
 public class ResourcesRegistrar extends ResourceConfig {
 
 	public ResourcesRegistrar() {
 		register(GsonMessageBodyHandler.class);
 		register(MultiPartFeature.class);
+		register(CustomResponseFilter.class);
 	}
 
 }
